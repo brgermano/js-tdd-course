2df1de1d70d223826b176f609191ba471a584e6a
Bruno Germano <brunoalvesgermano@gmail.com>
2020-12-04 23:33:46 -0300

    Add comments

--- es6-module/app.js
+++ es6-module/app.js
@@ -1,3 +1,6 @@
+// import metodo from 'biblioteca';
+// * carrega tudo da lib
+// as => alias para método (novo nome)
 import * as R from 'ramda';
 
 const arr1 = [1, 1, 1, 2, 2, 3, 4, 5, 6, 6];
@@ -5,5 +8,10 @@ const arr2 = [5, 6, 6, 6, 7, 7, 8, 9, 10, 1];
 
 const arr3 = R.union(arr1, arr2);
 
+const arr4 = R.uniq(arr1);
+
 // eslint-disable-next-line no-console
 console.log(arr3);
+
+// eslint-disable-next-line no-console
+console.log(arr4);
