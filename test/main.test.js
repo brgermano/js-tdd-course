96467b6f54ba96ec2bacf84bf76afc29ffb0daed
Bruno Germano <brunoalvesgermano@gmail.com>
2021-02-16 20:51:45 -0300

    Add a mocha reporter to npm test script

--- test/main.test.js
+++ test/main.test.js
@@ -1,8 +1,42 @@
-var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1, 2, 3].indexOf(4), -1);
+/*
+describe separa os grandes blocos
+
+dentro de um describe podemos criar outros
+para testar os metodos por exemplo
+
+e dentro desses sub-describes podemos criar os
+contextos
+
+it é oq de fato vai rodar
+
+*/
+/* eslint-disable */
+describe('Main', () => {
+  describe('Method A', () => {
+    context('Case 1', () => {
+      it('sould happen sum 2 with 2 and return 4', () => {
+        // espera que aconteça
+        // Entra de dados / metodo sum(2,2)
+        // Espera retornar (4) => true | (3) => false => broken test
+      });
+    });
+
+    context('Case 2', () => {
+      it('sould happen mimi', () => {
+        // espera que aconteça
+        // Entra de dados / metodo sum(2,2)
+        // Espera retornar (4) => true | (3) => false => broken test
+      });
+
+      it('sould happen blabla', () => {
+        // espera que aconteça
+        // Entra de dados / metodo sum(2,2)
+        // Espera retornar (4) => true | (3) => false => broken test
+      });
     });
   });
-});
+
+  describe('Method B', () => {
+    
+  });
+})
